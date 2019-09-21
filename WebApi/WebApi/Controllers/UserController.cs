8c011d57c85bd60d231898b514d6b5367b140362
[Wittawat] <[Wittawat335@gmail.com]>
2019-09-21 17:53:51 +0700

    login medtod

--- WebApi/WebApi/Controllers/UserController.cs
+++ WebApi/WebApi/Controllers/UserController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -39,6 +43,42 @@ namespace WebApi.Controllers
                 return Ok(res);
             }
             catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/User/Login
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                        {
+                            new Claim("UserID", user.Id.ToString())
+                        }),
+                        Expires = DateTime.UtcNow.AddMinutes(5),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("12132345655854")), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                    var token = tokenHandler.WriteToken(securityToken);
+
+                    return Ok(new { token });
+                }
+                else
+                {
+                    return BadRequest(new { message = "username or password is incorrect." });
+                }
+            }
+            catch(Exception ex)
             {
                 throw ex;
             }
